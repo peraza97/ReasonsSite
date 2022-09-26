598d7863108077d049742413f855a3fa75b4504b
peraza97 <omarperaza19@gmail.com>
2022-09-25 17:45:11 -0700

    update site

--- Frontend/src/main.js
+++ Frontend/src/main.js
@@ -1,14 +1,56 @@
 const instance = new AwsApi();
 Object.freeze(instance);
 
+//#region Login
+function showPassword() {
+    var x = document.getElementById("PassField");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+}
+
+async function GetToken(userName, password) {
+    try{
+        const _body = {
+            "AuthParameters": {
+                "USERNAME" : userName,
+                "PASSWORD" : password
+            },
+            "AuthFlow" : "USER_PASSWORD_AUTH",
+            "ClientId" : "5e5jou42audpmnpnj666f0fk93"
+        }
+        const response = await fetch('https://cognito-idp.us-east-2.amazonaws.com:443',{
+            method : "POST",
+            headers : {
+                "Content-type": "application/x-amz-json-1.1",
+                "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth"
+            },
+            body : JSON.stringify(_body),
+        });
+
+        const result = await response.json(); 
+        
+        return result.AuthenticationResult.AccessToken;
+    }
+    catch (error) {
+        console.log(error)
+    }
+
+    return "";
+}
+
 function CheckCookie() {
     if (GetCookie("reasonsToken") == ""){
         console.log("Cookie not present");
+        if (window.location.pathname.split('/').pop() != 'index.html') {
+            NavigateToPage('index.html')
+        }
+        return false
     }
-    else {
-        console.log("Cookie present");
-        HideLoginScreen();
-    }
+
+    return true
 }
 
 async function Login(userName, password) {
@@ -20,24 +62,40 @@ async function Login(userName, password) {
     else {
         // Set the cookie 
         SetCookie("reasonsToken", token, 1);
-        HideLoginScreen();
+        CompleteLogin();
     }
 }
 
-function HideLoginScreen() {
+function CompleteLogin(){
     // hide the login section
     document.getElementById("login").style.display = "none";
-
-    admin = location.href.substring(location.href.lastIndexOf("?")+1);
-
-    if (admin == "admin") {
-        // Show the admin buttons
-        document.getElementById("AdminSection").style.display = "block";
-    }
+    
     // Show the reasons buttons
-    document.getElementById("ReasonSection").style.display = "block";
+    document.getElementById("menu").style.display = "block";
+}
+//#endregion
+
+//#region validation of page login
+// Validation for index.html
+function IndexPagePreValidation() {
+    if (CheckCookie() == true) {
+        CompleteLogin();
+    }
 }
 
+// Validation for index.html
+function ReasonsPagePreValidation() {
+    if (CheckCookie() == true) {
+        const urlSearchParams = new URLSearchParams(window.location.search);
+        const myParam = urlSearchParams.get('admin');
+        if (myParam == 'true') {
+            document.getElementById("AdminSection").style.display = "block";
+        }
+    }
+}
+//#endregion
+
+//#region Display text in view
 function ShowText(reasonTextBox, text) {
     reasonTextBox.style.display = "block";
     reasonTextBox.innerHTML  = text
@@ -45,15 +103,6 @@ function ShowText(reasonTextBox, text) {
 
 function ShowMaintenanceText(reasonTextBox) {
     ShowText(reasonTextBox,"<img src=\"imgs/workingBean.png\" width=\"45\" height=\"35\" style=\"margin: 0px 10px;vertical-align: middle;\">Contact your nearest bean</img>");
-}
-
-function showPassword() {
-    var x = document.getElementById("PassField");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
 }
 
 async function LoadReasonView() {
@@ -97,33 +146,14 @@ async function AddReasonView(reason) {
     
     document.getElementById('AddField').value = ''
 }
+//#endregion
 
-async function GetToken(userName, password) {
-    try{
-        const _body = {
-            "AuthParameters": {
-                "USERNAME" : userName,
-                "PASSWORD" : password
-            },
-            "AuthFlow" : "USER_PASSWORD_AUTH",
-            "ClientId" : "5e5jou42audpmnpnj666f0fk93"
-        }
-        const response = await fetch('https://cognito-idp.us-east-2.amazonaws.com:443',{
-            method : "POST",
-            headers : {
-                "Content-type": "application/x-amz-json-1.1",
-                "X-Amz-Target": "AWSCognitoIdentityProviderService.InitiateAuth"
-            },
-            body : JSON.stringify(_body),
-        });
-
-        const result = await response.json(); 
-        
-        return result.AuthenticationResult.AccessToken;
+function NavigateToPage(htmlPage) {
+    var newPage = htmlPage;
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const myParam = urlSearchParams.get('admin');
+    if (myParam == 'true') {
+        newPage += '?admin=true'
     }
-    catch (error) {
-        console.log(error)
-    }
-
-    return "";
+    window.location = newPage
 }
